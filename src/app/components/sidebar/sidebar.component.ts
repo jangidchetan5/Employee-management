d02c391ca4e203c75ea3a3593c570a0f62f79529
Chetan Jangid <jangid.chetan5@gmail.com>
2021-09-23 22:25:50 +0530

    new update

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -4,7 +4,8 @@ import { AuthService } from 'src/app/core/services/auth.service';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
+  
 })
 export class SidebarComponent implements OnInit {
 
